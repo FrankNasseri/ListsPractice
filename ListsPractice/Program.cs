ca34b63e77e4341cc2c7ed3acc4b423926b8c229
Frank Nasseri <franknasseri57@gmail.com>
2017-07-12 10:52:10 -0400

    Added list problems

--- ListsPractice/Program.cs
+++ ListsPractice/Program.cs
@@ -10,6 +10,69 @@ namespace ListsPractice
     {
         static void Main(string[] args)
         {
+            //List<int> favNumbers = new List<int>();
+            //favNumbers.Add(7);
+            //favNumbers.Add(25);
+            //favNumbers.Add(30);
+
+            //foreach(int number in favNumbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            ////Use the Count property to get the length of the list
+            //Console.WriteLine(favNumbers.Count);
+
+            //List<string> books = new List<string> {"Cat in the hat", "Toad and frog", "Green eggs and ham" };
+            //Console.WriteLine(books[0]);
+            ////Inserts at the current index moving the previous element down one
+            //books.Insert(0, "Horton hears a Who");
+            //Console.WriteLine(books[0]);
+
+            //Create a List and add five animals using the .Add()
+            //Print each animal in the list
+            //List<string> favAnimals = new List<string>();
+            //favAnimals.Add("Monkey");
+            //favAnimals.Add("Cats");
+            //favAnimals.Add("Penguins");
+            //favAnimals.Add("Panthers");
+            //favAnimals.Add("Jelly Fish");
+
+            //foreach(string animal in favAnimals)
+            //{
+            //    Console.WriteLine(animal);
+            //}
+            ////OR, WITH THE SAME FAV ANIMALS, just using a for loop instead of foreach loop
+            //for(int i= 0; i< favAnimals.Count; i++)
+            //{
+            //    Console.WriteLine(favAnimals[i]);
+            //}
+
+            //Create the following list:
+            //A bool list {true,false,false,true,false}
+            //Loop through each value 
+            //If the value is true print "Better bring an umbrella"
+            //If the value is false print "No rain today enjoy the sun"
+            //List<bool> weather = new List<bool> { true, false, false, true, false };
+            //foreach(bool day in weather)
+            //{
+            //    if(day == true)
+            //    {
+            //        Console.WriteLine("Better bring an umbrella");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("No rain today, enjoy the sun");
+            //    }
+            //}
+
+            //Create a list of our favorite foods using a .Contains
+            List<string> faveFoods = new List<string> { "Steak", "Fish", "Katsudon", "Ice Cream", "Nachos" };
+
+            if(faveFoods.Contains("Fish"))
+            {
+                Console.WriteLine("I like fish too!");
+            }
+
         }
     }
 }
